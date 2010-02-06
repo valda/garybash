99172d4ed4977108c05b04adab7c91f861e1c635
YAMAGUCHI Seiji <valda@underscore.jp>
2010-02-06 18:54:41 +0900

    Fixed MreKeym(key record).

--- Mopy/bosh.py
+++ Mopy/bosh.py
@@ -3166,14 +3166,31 @@ class MreIngr(MelRecord,MreHasEffects):
 
 #------------------------------------------------------------------------------
 class MreKeym(MelRecord):
-    """MISC (miscellaneous item) record."""
+    """KEYM (key) record."""
     classType = 'KEYM'
     melSet = MelSet(
         MelString('EDID','eid'),
+        MelStruct('OBND','=6h',
+                  'corner0X','corner0Y','corner0Z',
+                  'corner1X','corner1Y','corner1Z'),
         MelString('FULL','full'),
         MelModel(),
-        MelString('ICON','iconPath'),
+        MelString('ICON','largeIconPath'),
+        MelString('MICO','smallIconPath'),
         MelFid('SCRI','script'),
+        MelGroup('destructable',
+                 MelBase('DEST','header'),
+                 MelStruct('DEST','IhH','health','count','flags'),
+                 MelGroups('stages',
+                           MelStruct('DSTD','=4B4I','health','index','damageStage','flags',
+                                     'selfDamagePerSecond',(FID,'explosion',None),(FID,'debris',None),'debrisCount'),
+                           MelString('DMDL','model'),
+                           MelBase('DMDT','_dmdt'), #--Should be a struct. Maybe later.
+                           MelBase('DSTF','footer'),
+                           ),
+                 ),
+        MelFid('YNAM','soundPickUp'),
+        MelFid('ZNAM','soundDrop'),
         MelStruct('DATA','if','value','weight'),
         )
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
