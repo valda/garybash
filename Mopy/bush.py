3483b108ad59659c356e6a2cb3617d0739a4da40
YAMAGUCHI Seiji <valda@underscore.jp>
2010-05-02 14:08:45 +0900

    add 'GetWeaponHasScope'(FOSE function) to conditionFunctionData.

--- Mopy/bush.py
+++ Mopy/bush.py
@@ -424,6 +424,7 @@ conditionFunctionData = ( #--0: no param; 1: int param; 2: formid param
     (455, 'GetPlayerTeammateCount', 0, 0, 0, 0),
     (219, 'GetAnimAction', 0, 0, 0, 0),
     (430, 'IsActorTalkingThroughActivator', 0, 0, 0, 0),
+    (1165, 'GetWeaponHasScope', 0, 0, 0, 0),
     )
 allConditions = set(entry[0] for entry in conditionFunctionData)
 fid1Conditions = set(entry[0] for entry in conditionFunctionData if entry[2] == 2)
