e31b566d1405379b64ac8c802b506445cd6cbc13
YAMAGUCHI Seiji <valda@underscore.jp>
2010-11-06 18:23:22 +0900

    Updated 'masterupdate setup.bat' for FNVEdit.

--- Mopy/Extras/masterupdate setup.bat
+++ Mopy/Extras/masterupdate setup.bat
@@ -1,6 +1,6 @@
-rem This batch file will set up MasterUpdate and MasterRestor. 
-rem Place it into the same directory with FO3Edit.exe and activate it.
-rem FO3Edit.exe link http://www.fallout3nexus.com/downloads/file.php?id=637
+rem This batch file will set up MasterUpdate and MasterRestore.
+rem Place it into the same directory with FNVEdit.exe and activate it.
+rem FNVEdit.exe link http://newvegasnexus.com/downloads/file.php?id=34703
 
-copy FO3Edit.exe FO3MasterRestore.exe
-copy FO3Edit.exe FO3MasterUpdate.exe
+copy FNVEdit.exe FNVMasterRestore.exe
+copy FNVEdit.exe FNVMasterUpdate.exe
