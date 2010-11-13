f71802e0389a5eb7d43be605ed387e795b6ead00
YAMAGUCHI Seiji <valda@underscore.jp>
2010-11-13 19:44:22 +0900

    Fixed a bug that memory error is thrown at the bsafile scan.

--- Mopy/bosh.py
+++ Mopy/bosh.py
@@ -8726,7 +8726,7 @@ class BsaFile:
         ins = bolt.StructFile(self.path.s,'rb')
         #--Header
         ins.seek(4*4)
-        (self.folderCount,self.fileCount,lenFolderNames,lenFileNames,fileFlags,fileFlags2) = ins.unpack('6I',recHeaderSize)
+        (self.folderCount,self.fileCount,lenFolderNames,lenFileNames,fileFlags) = ins.unpack('5I',20)
         #--FolderInfos (Initial)
         folderInfos = self.folderInfos = []
         for index in range(self.folderCount):
@@ -8788,7 +8788,7 @@ class BsaFile:
         bsaTimes = (
             ('Fallout - Meshes.bsa',1138575220),
             ('Fallout - Misc.bsa',1139433736),
-            ('Fallout - Sounds.bsa',1138660560),
+            ('Fallout - Sound.bsa',1138660560),
             ('Fallout - Textures.bsa',1138162634),
             ('Fallout - Textures2.bsa',1138162934),
             ('Fallout - Voices1.bsa',1138166742),
