b455cee50182558e3a086a76cdd930e9574941e1
YAMAGUCHI Seiji <valda@underscore.jp>
2011-07-26 13:46:42 +0900

    ディレクトリ階層を変更

--- Mopy/Extras/masterupdate setup.bat
+++
@@ -1,6 +0,0 @@
-rem This batch file will set up MasterUpdate and MasterRestore.
-rem Place it into the same directory with FNVEdit.exe and activate it.
-rem FNVEdit.exe link http://newvegasnexus.com/downloads/file.php?id=34703
-
-copy FNVEdit.exe FNVMasterRestore.exe
-copy FNVEdit.exe FNVMasterUpdate.exe
